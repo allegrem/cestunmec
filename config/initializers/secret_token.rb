fb11f0eb15f8be31efbe4451b2f63de293340edc
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-02-17 20:54:28 +0100

    Commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cestunmec::Application.config.secret_token = 'bf61d393dc37378cfda73ce5aa8a08e945b7f31e11cf79cc7428e859890d6ea9af7605a6638e20f240cd9435072a583172fbdfbe0d4d10ae3b0b5b040bac4d3b'
