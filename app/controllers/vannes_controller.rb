b4560a8bc62200890c8f400eb319dcb2df0d8de3
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-02-19 18:10:39 +0100

    Ajout de Les buzzantes (a ameliorer)

--- app/controllers/vannes_controller.rb
+++ app/controllers/vannes_controller.rb
@@ -11,6 +11,8 @@ class VannesController < ApplicationController
       case params[:order]
 	when 'best'
 	  @vannes = Vanne.order('lols_count DESC, created_at DESC').limit(10).offset(10*params[:page].to_i)
+	when 'buzz'
+	  @vannes = Vanne.order('lols_count/created_at DESC').limit(10).offset(10*params[:page].to_i)
 	else
 	  @vannes = Vanne.order('created_at DESC').limit(10).offset(10*params[:page].to_i)
       end
