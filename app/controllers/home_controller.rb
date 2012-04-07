5543306783ab4cc1927311897647c89cd443974c
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-04-07 22:25:27 +0200

    premier jet random vannes sur index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,8 @@ class HomeController < ApplicationController
     
     @direct_vannes = Vanne.where("valide=?",true).order("created_at DESC").limit(10)
     @lol_vannes = Vanne.where("valide=?",true).order("lols_count DESC, created_at DESC").limit(10)
+    
+    vannes_ids = Vanne.find( :all, :select => 'id' ).map( &:id )
+    @random_vannes = Vanne.find( (1..10).map { vannes_ids.delete_at( vannes_ids.size * rand ) } )
   end
 end
