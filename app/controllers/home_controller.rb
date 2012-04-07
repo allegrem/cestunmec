6c523838492e2c1301542cd0680c333aa15ec8ee
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-04-08 00:08:56 +0200

    meilleur partial membre + modifications mineures layout principal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,11 @@ class HomeController < ApplicationController
     @nbre_vanneurs = Membre.count
     @nbre_vannes_postees = Vanne.where("valide=?",true).count
     
-    @direct_vannes = Vanne.where("valide=?",true).order("created_at DESC").limit(10)
-    @lol_vannes = Vanne.where("valide=?",true).order("lols_count DESC, created_at DESC").limit(10)
+    @direct_vannes = Vanne.where("valide=?",true).order("created_at DESC").limit(7)
+    @lol_vannes = Vanne.where("valide=?",true).order("lols_count DESC, created_at DESC").limit(7)
     
     vannes_ids = Vanne.find( :all, :select => 'id' ).map( &:id )
-    @random_vannes = Vanne.find( (1..10).map { vannes_ids.delete_at( vannes_ids.size * rand ) } )
+    @random_vannes = Vanne.find( (1..7).map { vannes_ids.delete_at( vannes_ids.size * rand ) } )
     
     @vip_vanneurs = Membre.order("lols_count * vannes_count DESC").limit(10)
   end
