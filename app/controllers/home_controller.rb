cdb4d6737b4ffebc00942d99595ca6bf4ac8fbdc
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-04-07 22:18:41 +0200

    direct-vannes, lol of fame sur la page d'accueil + debut du partial vannes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
     @membre = Membre.new
+    
     @nbre_vanneurs = Membre.count
     @nbre_vannes_postees = Vanne.where("valide=?",true).count
+    
+    @direct_vannes = Vanne.where("valide=?",true).order("created_at DESC").limit(10)
+    @lol_vannes = Vanne.where("valide=?",true).order("lols_count DESC, created_at DESC").limit(10)
   end
 end
