1727f123cc8eb4861f93434e1cce891866979f44
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-04-07 21:43:35 +0200

    fin du hero unit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @membre = Membre.new
+    @nbre_vanneurs = Membre.count
+    @nbre_vannes_postees = Vanne.where("valide=?",true).count
   end
 end
