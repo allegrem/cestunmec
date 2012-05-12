762deab48fb5f0a19faabcdb7670945be1c50ff3
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-05-13 00:32:35 +0200

    fix bug vannes non valides s'affichent sur la page d'accueil

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
     @direct_vannes = Vanne.where("valide=?",true).order("created_at DESC").limit(7)
     @lol_vannes = Vanne.where("valide=?",true).order("lols_count DESC, created_at DESC").limit(7)
     
-    vannes_ids = Vanne.find( :all, :select => 'id' ).map( &:id )
+    vannes_ids = Vanne.where("valide=?",true).select("id").map( &:id )
     @random_vannes = Vanne.find( (1..7).map { vannes_ids.delete_at( vannes_ids.size * rand ) } )
     
     @vip_vanneurs = Membre.order("lols_count * vannes_count DESC").limit(10)
