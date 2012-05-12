c748136b443456939512a4c5f17f2032fbf7ddf5
Matthieu Allegre <matthieu.allegre@gmail.com>
2012-05-12 20:18:10 +0200

    Ajout d'un bouton nouvelle vanne dans la navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
     @membre = Membre.new
     
+    @vanne = Vanne.new
+    
     @nbre_vanneurs = Membre.count
     @nbre_vannes_postees = Vanne.where("valide=?",true).count
     
